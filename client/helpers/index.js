8eaf972dc818ad3dec5214a991962b3c4fa42d62
David Alonso <david.alonso@local.eurecat.org>
2016-09-29 17:22:13 +0200

    New landing page

---
+++ client/helpers/index.js
@@ -0,0 +1,32 @@
+Template.index.onRendered(function(){
+  var validator = $('.login').validate({
+    submitHandler: function(event){
+      var loginObject = {
+        email: $('[name=email]').val(),
+        password: $('[name=password]').val()
+      };
+
+      Meteor.call('loginWithPassword', loginObject, function(error, response){
+        if(! error){
+          //success
+          Session.setPersistent('person', response);
+          Session.setPersistent('user', response);
+          Router.go('home');
+        } else {
+          console.log('An error happens...');
+          console.log(error);
+
+          validator.showErrors({
+            password: "Your email or password is incorrect."
+          });
+        }
+      });
+    }
+  });
+});
+
+Template.index.events({
+  'submit form': function(event){
+    event.preventDefault();
+  }
+});
